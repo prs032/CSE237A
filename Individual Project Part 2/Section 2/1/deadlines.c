1006724f93f672dcd58cf2169cd9e3d385ac84b6
Prashant Singh <prs032@ucsd.edu>
2017-05-26 14:56:13 -0700

    Create deadlines.c

---
+++ Individual Project Part 2/Section 2/1/deadlines.c
@@ -0,0 +1,13 @@
+// Generated deadlines for the test workload
+// This file has to be included only in main_week4.c
+long long workloadDeadlines[] = {
+	200000,
+	600000,
+	500000,
+	600000,
+	400000,
+	500000,
+	200000,
+	150000,
+};
+
